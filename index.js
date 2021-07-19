7cff038459c617bc659f2fbcbbe58dbe8f9125ff
sechs <philippe.buteau@skema.edu>
2021-07-19 19:09:48 +0200

    Implemented the icons on the skills section

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const header = document.querySelector("header");
 const shadow = document.querySelector(".shadow");
 const content = document.querySelector(".content");
 const aboutSection = document.querySelector(".about");
-const skillsSection = document.querySelector(".skills");
+const skillsSection = document.querySelector(".skills .container");
 const imgContainer = document.querySelector(".img-container");
 const opacity = document.querySelectorAll(".opacity");
 const border = document.querySelector(".border");
+const langIcon = document.querySelector(".lang-icon");
 
 let headerHeight = header.offsetHeight;
 let aboutSectionHeight = aboutSection.offsetHeight;
@@ -19,8 +20,10 @@ console.log("this is sectionHeight", aboutSectionHeight);
 window.addEventListener("scroll", () => {
   let scroll = window.pageYOffset;
   let aboutSectionY = aboutSection.getBoundingClientRect();
+  let skillsSectionY = skillsSection.getBoundingClientRect();
   console.log("this is the scroll", scroll);
   console.log("this is sectionYtop", aboutSectionY.top);
+  console.log(border.style.width);
   // console.log("this is sectionYbottom", sectionY.bottom);
 
   translate.forEach((element) => {
@@ -36,14 +39,19 @@ window.addEventListener("scroll", () => {
   shadow.style.height = `${scroll * 0.5 + 10}px`;
 
   content.style.transform = `translateY(${
-    (scroll / (aboutSectionHeight + aboutSectionY.top)) * 60 - 60
+    (scroll / (aboutSectionHeight + aboutSectionY.top)) * -60 + 60
   }px)`;
 
   imgContainer.style.transform = `translateY(${
-    (scroll / (aboutSectionHeight + aboutSectionY.top)) * -50 + 50
+    (scroll / (aboutSectionHeight + aboutSectionY.top)) * -60 + 60
   }px)`;
 
   border.style.width = `${
-    (scroll / (aboutSectionY.top + aboutSectionHeight)) * 30
+    (scroll / (aboutSectionY.top + aboutSectionHeight)) * 25
   }%`;
+
+  skillsSection.style.transform = `translateY(${
+    (scroll / (skillsSectionHeight + skillsSectionY.top)) * -60 + 60
+  }px)`;
+  
 });
